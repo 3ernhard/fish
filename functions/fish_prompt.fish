b9049e35e055acc636eb11881f86b9446de5da41
3ernhard <bernhard-hoerl@gmx.at>
2026-03-04 16:35:33 +0100

    040326 rm SSH check from prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,8 +2,6 @@ function fish_prompt
     if set -q CONTAINER_ID
         echo -n "($CONTAINER_ID) "
     end
-    if test -n "$SSH_TTY"
     echo -n (set_color --bold green)"$USER"'@'(prompt_hostname)(set_color normal):
-    end
     echo -n (set_color --bold blue)(prompt_pwd)(set_color normal)'> '
 end
