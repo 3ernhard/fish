d891884414a74de31ee27936da4d8b096652e458
3ernhard <bernhard-hoerl@gmx.at>
2026-03-05 20:46:03 +0100

    added some aliases

--- conf.d/abbr.fish
+++ conf.d/abbr.fish
@@ -1,15 +1,24 @@
 abbr cls clear
 
+abbr q exit
+
 abbr md mkdir -p
 abbr rd rmdir -p
 
+# on debian fd = fdfind
 abbr fdh fd --hidden
 
+# copy and paste to system clipboard
 abbr yank wl-copy
 abbr spank wl-paste
 
+# vim
 abbr vi vimx
 abbr vim vimx
 
-abbr q exit
-abbr :q exit
+# .rmn to .pdf
+abbr rmn2pdf rcu --render-rmn-pdf-b
+
+# download from youtube
+abbr yt2mp3 yt-dlp -x --audio-format mp3
+abbr yt2mp4 yt-dlp --format mp4
