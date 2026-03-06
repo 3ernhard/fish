d1109425bfd3a0ed11b03c40927467bec76e84ae
3ernhard <bernhard-hoerl@gmx.at>
2026-03-06 12:29:29 +0100

    optimized prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,31 +1,36 @@
 function fish_prompt
-
-    set -l last_status $status
-    set -l duration (math -s2 $CMD_DURATION / 1000)
-    if test $duration -gt 1; or test $last_status -ne 0
+    if set -q _PROMPT_SHOWN
         echo -n (set_color brblack)(date +%H:%M:%S)(set_color normal)
-        if test $duration -gt 1
-            echo -n " "(set_color yellow)"$duration""s"(set_color normal)
+
+        set -l duration $CMD_DURATION
+        if test $duration -gt 1000
+            echo -n " "(set_color yellow)(math -s2 $duration / 1000)"s"(set_color normal)
         end
+
+        set -l last_status $status
         if test $last_status -ne 0
-            echo -n " "(set_color --bold red)"[$last_status]"(set_color normal)
+            echo -n " "(set_color --bold red)"["$last_status"]"(set_color normal)
         end
+
         echo ""
+        echo ""
+    else
+        set -g _PROMPT_SHOWN 1
     end
 
     if set -q CONTAINER_ID
-        echo -n "($CONTAINER_ID) "
+        echo -n "("$CONTAINER_ID") "
     end
     if set -q VIRTUAL_ENV
-        echo -n "(basename $VIRTUAL_ENV) "
+        echo -n "("basename $VIRTUAL_ENV") "
     end
     if set -q CONDA_DEFAULT_ENV
-        echo -n "($CONDA_DEFAULT_ENV) "
+        echo -n "("$CONDA_DEFAULT_ENV") "
     end
     if test "$HOSTNAME" = "toolbx"
         echo -n (set_color purple)"⬢ "(set_color normal)
     end
 
-    echo -n (set_color --bold green)"$USER"'@'(prompt_hostname)(set_color normal):
-    echo -n (set_color --bold blue)(prompt_pwd)(set_color normal)'> '
+    echo -n (set_color --bold green)$USER"@"(prompt_hostname)(set_color normal)":"
+    echo -n (set_color --bold blue)(prompt_pwd)(set_color normal)"> "
 end
